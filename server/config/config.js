2cc4086a0ead040a6c78e6cd68f002bc101a9ac8
Akriti Gupta Pratap <akritiguptapratap@Akritis-MacBook-Air.local>
2016-08-23 20:12:10 +0800

    Updating production DB info

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
 		
 	},
 	production:{
-		db: 'mongodb://localhost/homephilosophy',
+		db: 'mongodb://heroku_n2fg9cq5:6fm4i58g1v5l9n4d2p1uadiq7g@ds013966.mlab.com:13966/heroku_n2fg9cq5',
 		rootPath: rootPath,
 		port: process.env.PORT || 80
 	}
