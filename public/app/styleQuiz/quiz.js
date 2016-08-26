00d7f5817834c43e9e97c207569409604fdaf36c
Akriti Gupta Pratap <akritiguptapratap@Akritis-MacBook-Air.local>
2016-08-26 18:46:00 +0800

    Media Queries

--- public/app/styleQuiz/quiz.js
+++ public/app/styleQuiz/quiz.js
@@ -1,3 +1,29 @@
+$(document).ready(function(){
+	// alert('Doc ready');
+var touch = window.ontouchstart
+            || (navigator.MaxTouchPoints > 0)
+            || (navigator.msMaxTouchPoints > 0);
+// alert(touch);
+if (touch) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                	// alert('got hover: '+styleSheet);
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
+});
+
+
 //Image/ Heart Click on Pinterest Board
 
 			$(document).on('click','.thumbnail',function(e){
