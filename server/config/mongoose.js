466ef115c67e78cd8484909ff20d3100edd384a5
Akriti Gupta Pratap <guptaakriti83@gmail.com>
2016-08-27 23:36:36 +0800

    mongo log to check on server

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,10 +2,15 @@ var mongoose = require('mongoose'),
 	userModel = require('../models/User');
 
 module.exports = function(config){
+	console.log('In mongoose file, config.db is: ');
 	console.log(config.db)
+	console.log('Before connecting to DB');
 	mongoose.connect(config.db);
+	console.log('after connecting to DB');
 	var db = mongoose.connection;
-	db.on('error',console.error.bind(console,'connection error'));
+	console.log('db is: '+db);
+	// db.on('error',console.error.bind(console,'connection error'));
+	db.on('error',console.log('connection error'));
 	db.once('open',function(callback){
 		console.log("HP DB opened");
 	});
