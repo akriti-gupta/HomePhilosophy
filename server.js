fda734c338762f43b7c3a7ca2ddc7df85476d15d
Akriti Gupta Pratap <guptaakriti83@gmail.com>
2016-09-27 06:01:29 +0800

    Select box style

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');	
 
-var env = process.env.NODE = process.env.NODE || 'production';
+var env = process.env.NODE = process.env.NODE || 'development';
 console.log(env);
 var app = express();
 var config = require('./server/config/config')[env];
