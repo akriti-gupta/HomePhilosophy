df84a2d4938e928a975e670b57e7bbabbb54a088
Akriti Gupta Pratap <guptaakriti83@gmail.com>
2017-01-13 17:56:59 +0800

    Removing bookshelf code from app.js

--- public/app/app.js
+++ public/app/app.js
@@ -4,16 +4,16 @@ angular.module('app',['ngResource','ngRoute','angularGrid','ngFileUpload', 'ui.b
 angular.module('app')
 			.config(function($routeProvider, $locationProvider){
 
-				var routeRoleChecks ={
-					admin:{auth: function(mvAuth){
-								return mvAuth.authorizeUserForRoute('admin');
-							}}
-				};
-				var routeLoginChecks ={
-					user:{login: function(mvAuth){
-								return mvAuth.authorizeUserForLogin();
-							}}
-				};
+				// var routeRoleChecks ={
+				// 	admin:{auth: function(mvAuth){
+				// 				return mvAuth.authorizeUserForRoute('admin');
+				// 			}}
+				// };
+				// var routeLoginChecks ={
+				// 	user:{login: function(mvAuth){
+				// 				return mvAuth.authorizeUserForLogin();
+				// 			}}
+				// };
 				$locationProvider.html5Mode(true);
 				$routeProvider
 					.when('/',{templateUrl: 'app/home/main.html', controller: 'MainController'})
@@ -27,16 +27,19 @@ angular.module('app')
 					.when('/style-quiz',{templateUrl: 'app/styleQuiz/styleQuiz.html', controller: 'QuizController'})
 					.when('/login',{templateUrl: 'app/account/login.html', controller: 'LoginController'})
 					.when('/signup',{templateUrl: 'app/account/login.html', controller: 'LoginController'})
-					.when('/tell-us-more',{templateUrl: 'app/moreDetails.html', controller: 'SaveDetails',resolve: routeLoginChecks.user})
-					.when('/reviewPayment',{templateUrl: 'app/home/payment/reviewPayment.html', controller: 'PaymentController',resolve: routeLoginChecks.user})
-					.when('/admin/users',{templateUrl: 'app/admin/userList.html', 
-						controller: 'UserListCtrl',resolve: routeRoleChecks.admin})
-					.when('/dashboard',{templateUrl: 'app/customer/dashboard.html', controller: 'CustViewController',resolve: routeLoginChecks.user})
+					.when('/tell-us-more',{templateUrl: 'app/moreDetails.html', controller: 'SaveDetails'})
+					// .when('/tell-us-more',{templateUrl: 'app/moreDetails.html', controller: 'SaveDetails',resolve: routeLoginChecks.user})
+					// .when('/reviewPayment',{templateUrl: 'app/home/payment/reviewPayment.html', controller: 'PaymentController',resolve: routeLoginChecks.user})
+					.when('/reviewPayment',{templateUrl: 'app/home/payment/reviewPayment.html', controller: 'PaymentController'})
+					// .when('/admin/users',{templateUrl: 'app/admin/userList.html', controller: 'UserListCtrl',resolve: routeRoleChecks.admin})
+					.when('/admin/users',{templateUrl: 'app/admin/userList.html', controller: 'UserListCtrl'})
+					// .when('/dashboard',{templateUrl: 'app/customer/dashboard.html', controller: 'CustViewController',resolve: routeLoginChecks.user})
+					.when('/dashboard',{templateUrl: 'app/customer/dashboard.html', controller: 'CustViewController'})
 					
 					.when('/feedback',{templateUrl: 'app/customer/feedback.html', controller: 'CustViewController'})
 
 					
-					.when('/projectDetail/:idx/detail/:detail',{templateUrl: 'app/admin/projectDetail.html', controller: 'ProjectDetailCtrl'})
+					// .when('/projectDetail/:idx/detail/:detail',{templateUrl: 'app/admin/projectDetail.html', controller: 'ProjectDetailCtrl'})
 					.otherwise({templateUrl: 'app/home/main.html', controller: 'MainController'});
     		});
 
