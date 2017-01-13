39f29d216d49f74e4498065cf19340ec10dd5917
Akriti Gupta Pratap <akritiguptapratap@Akritis-MacBook-Air.local>
2017-01-13 18:21:49 +0800

    Trying to remove booksheld dependency

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,46 +3,47 @@
   	 var User = ModelBase.extend({tableName: 'user'}); */
 
 var User = require('../models/User'),
-	Users = require('../collections/Users'),
-	CustQuiz = require('../models/CustQuiz'),
-	CustQuizzes = require('../collections/CustQuizzes'),
+	// Users = require('../collections/Users'),
+	// CustQuiz = require('../models/CustQuiz'),
+	// CustQuizzes = require('../collections/CustQuizzes'),
 	encryption = require('../utilities/encryption');
 	
 
 exports.getUsers = function(req,res){
-		// User.find({}).exec(function(err,collection){
-		// 	res.send(collection);
-		// });
+		User.find({}).exec(function(err,collection){
+			res.send(collection);
+		});
+		
 		// User.findAll().asCallback(function(err,collection){
-	Users.forge().fetch().then(function(collection) { 
-		res.send(collection);
-	});
+	// Users.forge().fetch().then(function(collection) { 
+	// 	res.send(collection);
+	// });
 }
 
 
-exports.createUser = function(req,res,next){
-	var userData = req.body;
-	userData.salt = encryption.createSalt();
-	userData.password = encryption.hashPswd(req.body.password,userData.salt);
-	User.forge(userData).save().then (function(user){
-	//User.create(userData).then(function(user){
-		//res.send(user);
-		req.login(user,function(err){
-			if(err){return next(err);}
-			res.send(user);
-		})
-	}).catch(function(err){
-		if(err.toString().indexOf('ER_DUP_ENTRY')>-1){
-			err = new Error('Username exists');
-		}
-		res.status(400);
-		return res.send({reason:err.toString()});
-	});
-} 
+// exports.createUser = function(req,res,next){
+// 	var userData = req.body;
+// 	userData.salt = encryption.createSalt();
+// 	userData.password = encryption.hashPswd(req.body.password,userData.salt);
+// 	User.forge(userData).save().then (function(user){
+// 	//User.create(userData).then(function(user){
+// 		//res.send(user);
+// 		req.login(user,function(err){
+// 			if(err){return next(err);}
+// 			res.send(user);
+// 		})
+// 	}).catch(function(err){
+// 		if(err.toString().indexOf('ER_DUP_ENTRY')>-1){
+// 			err = new Error('Username exists');
+// 		}
+// 		res.status(400);
+// 		return res.send({reason:err.toString()});
+// 	});
+// } 
 
 
 //MONGO Method
-/*exports.createUser = function(req,res,next){
+exports.createUser = function(req,res,next){
 	var userData = req.body;
 	userData.salt = encryption.createSalt();
 	userData.password = encryption.hashPswd(req.body.password,userData.salt);
@@ -59,4 +60,4 @@ exports.createUser = function(req,res,next){
 			res.send(user);
 		})
 	})
-} */
+} 
