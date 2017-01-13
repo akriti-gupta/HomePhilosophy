f6f814dd395127e1856ffe7e110f79b6c888da3c
Akriti Gupta Pratap <guptaakriti83@gmail.com>
2017-01-13 16:08:57 +0800

    Luxury spaces

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app',['ngResource','ngRoute','angularGrid','ngFileUpload']); 
+angular.module('app',['ngResource','ngRoute','angularGrid','ngFileUpload', 'ui.bootstrap']); 
 
 
 angular.module('app')
@@ -9,21 +9,35 @@ angular.module('app')
 								return mvAuth.authorizeUserForRoute('admin');
 							}}
 				};
+				var routeLoginChecks ={
+					user:{login: function(mvAuth){
+								return mvAuth.authorizeUserForLogin();
+							}}
+				};
 				$locationProvider.html5Mode(true);
 				$routeProvider
 					.when('/',{templateUrl: 'app/home/main.html', controller: 'MainController'})
+					
+					.when('/about-us',{templateUrl: 'app/home/aboutUs/aboutUs.html'})
+					.when('/pricing',{templateUrl: 'app/home/payment/pricing.html', controller: 'PaymentController'})
 					.when('/portfolio',{templateUrl: 'app/home/portfolio.html'})
-					.when('/aboutUs',{templateUrl: 'app/home/aboutUs/aboutUs.html'})
-					.when('/pricing',{templateUrl: 'app/home/payment/pricing.html'})
+					.when('/luxury-spaces',{templateUrl: 'app/home/luxurySpaces.html'})
 					.when('/faq',{templateUrl: 'app/home/faq/faq.html'})
-					.when('/contactUs',{templateUrl: 'app/contact/contactUs.html'})
-					.when('/login',{templateUrl: 'app/account/login.html', controller: 'LoginController'})
+					.when('/contact-us',{templateUrl: 'app/contact/contactUs.html'})
 					.when('/style-quiz',{templateUrl: 'app/styleQuiz/styleQuiz.html', controller: 'QuizController'})
-					.when('/op-process',{templateUrl: 'app/home/process.html'})					
-					.when('/tell-us-more',{templateUrl: 'app/moreDetails.html', controller: 'SaveDetails'})
+					.when('/login',{templateUrl: 'app/account/login.html', controller: 'LoginController'})
+					.when('/signup',{templateUrl: 'app/account/login.html', controller: 'LoginController'})
+					.when('/tell-us-more',{templateUrl: 'app/moreDetails.html', controller: 'SaveDetails',resolve: routeLoginChecks.user})
+					.when('/reviewPayment',{templateUrl: 'app/home/payment/reviewPayment.html', controller: 'PaymentController',resolve: routeLoginChecks.user})
 					.when('/admin/users',{templateUrl: 'app/admin/userList.html', 
 						controller: 'UserListCtrl',resolve: routeRoleChecks.admin})
-					.when('/signup',{templateUrl: 'app/account/login.html', controller: 'LoginController'});
+					.when('/dashboard',{templateUrl: 'app/customer/dashboard.html', controller: 'CustViewController',resolve: routeLoginChecks.user})
+					
+					.when('/feedback',{templateUrl: 'app/customer/feedback.html', controller: 'CustViewController'})
+
+					
+					.when('/projectDetail/:idx/detail/:detail',{templateUrl: 'app/admin/projectDetail.html', controller: 'ProjectDetailCtrl'})
+					.otherwise({templateUrl: 'app/home/main.html', controller: 'MainController'});
     		});
 
 
