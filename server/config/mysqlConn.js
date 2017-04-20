000d889649e023de0d7c654edbf2130dab8a4c4f
Akriti Gupta Pratap <guptaakriti83@gmail.com>
2017-04-20 15:02:29 +0800

    Pointing back to server

--- server/config/mysqlConn.js
+++ server/config/mysqlConn.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
 				host     : '127.0.0.1',
 				user     : 'root',
 				password : 'Schmooz@14',
-				database : 'testSchema',
+				database : 'HomePhilosophy',
 				charset  : 'utf8'
 			});
 
